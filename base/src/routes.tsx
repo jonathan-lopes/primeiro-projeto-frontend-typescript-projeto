0f8aa0f20d65d048e12097667e01879d6cc473c3
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2023-04-18 20:36:39 -0300

    add protected route

--- base/src/routes.tsx
+++ base/src/routes.tsx
@@ -1,12 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import useAuth from "./hooks/useAuth";
 import Main from "./pages/Main";
 import SignIn from "./pages/SingIn";
+
+type Props = {
+  redirectTo: string;
+};
+function ProtectedRoutes({ redirectTo }: Props) {
+  const { handleGetToken } = useAuth();
+
+  return handleGetToken() ? <Outlet /> : <Navigate to={redirectTo} />;
+}
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
-      <Route path="/main" element={<Main />} />
+
+      <Route element={<ProtectedRoutes redirectTo="/" />}>
+        <Route path="/main" element={<Main />} />
+      </Route>
     </Routes>
   );
 }
