56a76bdceac9cfc489306ae873a111a694edc2be
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2023-04-18 20:37:09 -0300

    add page sign in

--- base/src/pages/SingIn/index.tsx
+++ base/src/pages/SingIn/index.tsx
@@ -1,15 +1,70 @@
-import logo from "../../assets/logo.svg";
-import "./styles.css";
+import { FormEvent, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import useAuth from "../../hooks/useAuth";
+import api from "../../services/api";
+import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { handleAddToken, handleGetToken } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = handleGetToken();
+
+    if (token) {
+      navigate("/main");
+      return;
+    }
+  }, [handleGetToken, navigate]);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    try {
+      if (!email || !password) {
+        throw new Error("Email and password is required");
+      }
+
+      const response = await api.post("/login", {
+        email,
+        password,
+      });
+
+      const { accessToken } = response.data;
+
+      handleAddToken(accessToken);
+
+      navigate("/main");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="container container-sign-in">
       <div className="sign-in">
         <img src={logo} alt="Logo Cubos Academy" />
-        <form>
-          <input type="text" placeholder="E-mail" />
-          <input type="password" placeholder="Password" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
           <span>
             Não tem cadastro? <Link to="/sign-up">Clique aqui!</Link>
