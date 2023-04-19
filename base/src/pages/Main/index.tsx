1758a3362e9fb8401588fb3dbd041e31a7a5e752
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2023-04-18 23:51:32 -0300

    add page main

--- base/src/pages/Main/index.tsx
+++ base/src/pages/Main/index.tsx
@@ -1,9 +1,35 @@
-import './styles.css';
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import TeacherCard from "../../components/TeacherCard";
+import api from "../../services/api";
+import Teacher from "../../types/Teacher";
+import "./styles.css";
 
 function Main() {
+  const [allTeachers, setAllTeachers] = useState<Teacher[]>([]);
+
+  useEffect(() => {
+    loadTeachers();
+  }, []);
+
+  async function loadTeachers() {
+    try {
+      const { data } = await api.get("/teachers");
+      setAllTeachers([...data]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <Header />
+
+      <div className="main-teachers">
+        {allTeachers.map((teacher) => (
+          <TeacherCard key={teacher.id} teacher={teacher} />
+        ))}
+      </div>
     </div>
   );
 }
